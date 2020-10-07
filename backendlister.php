26ae8702943b9ce86389ceaa908b65c6f1b16115
Dominik Kreslo <dominik061997@gmail.com>
2020-10-06 23:23:48 -0400

    added backend listener file
    Signed-off-by: Dominik Kreslo <dominik061997@gmail.com>

--- backendlister.php
+++ backendlister.php
@@ -1,4 +1,15 @@
 <?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
 
+$server = new rabbitMQServer("rabbitMQ.ini", "database");
+echo "server started up";
+$server->process_requests('process');
 
+function sendRabbit($data, $server){
+    $client = new rabbitMQClient("rabbitMQ.ini",$server);
+    $response = $client->send_request($data);
+    return $response;
+}
 ?>
