da58ff8a4f26f6984e8bdcfc9a539c5f415ed13b
Dominik Kreslo <dominik061997@gmail.com>
2020-11-02 19:11:06 -0500

    Update mysqlconnect.php

--- mysqlconnect.php
+++ mysqlconnect.php
@@ -1,23 +1,23 @@
 #!/usr/bin/php
 <?php
 
-$mydb = new mysqli('127.0.0.1','testuser','12345','testMockingbird');
+$users = new mysqli('127.0.0.1','testuser','12345','testMockingbird');
 
-if ($mydb->errno != 0)
+if ($users->errno != 0)
 {
-    echo "failed to connect to database: ". $mydb->error . PHP_EOL;
+    echo "failed to connect to database: ". $users->error . PHP_EOL;
     exit(0);
 }
 
 echo "successfully connected to database".PHP_EOL;
 
-$query = "select * from usernames;";
+$query = "select * from users;";
 
-$response = $mydb->query($query);
-if ($mydb->errno != 0)
+$response = $users->query($query);
+if ($users->errno != 0)
 {
     echo "failed to execute query:".PHP_EOL;
-    echo __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
+    echo __FILE__.':'.__LINE__.":error: ".$users->error.PHP_EOL;
     exit(0);
 }
 
